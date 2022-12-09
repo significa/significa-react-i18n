1b98acc0ac3ad8f255fdebe53e6e2aaeccdd9411
Pedro Brandão <pedro@significa.co>
2022-12-09 10:12:24 +0000

    ci: new release flow

--- lib/types/context.d.ts
+++
@@ -1,8 +0,0 @@
-import React, { ReactNode } from 'react';
-export declare const I18nContext: React.Context<{
-    readonly messages: Record<string, string>;
-}>;
-export declare const I18nProvider: ({ messages, children, }: {
-    messages: Record<string, string>;
-    children: ReactNode;
-}) => JSX.Element;
--- lib/types/useTranslation.d.ts
+++
@@ -1,4 +0,0 @@
-import { Values } from './types';
-export declare const useTranslation: () => {
-    t: <K extends string>(key: K, ...args: [undefined?] | [any]) => string;
-};
